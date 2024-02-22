a82724ad1c7d076b39a9ac3fdb1fdebd57990139
KykyryzoB <148477723+KykyryzoB@users.noreply.github.com>
2024-02-22 23:27:32 +0500

    Update DONT LEAKED THIS SCRIPT.lua

--- DONT LEAKED THIS SCRIPT.lua
+++ DONT LEAKED THIS SCRIPT.lua
@@ -13,55 +13,59 @@ if game.PlaceId == 6403373529 then
 
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Kavo.lua"))()
 
-local Window = Library.CreateLib("stop stealing my leaked", "DarkTheme")
+local Window = Library.CreateLib("МОЙ ЮЗЕР В ТГ @MEi3_ZaHaRVi", "DarkTheme")
 
-    local Tab = Window:NewTab("I AM KYKYRYZ0B")
+    local Tab = Window:NewTab("ЧАТ Я ВАШИХ МАМ ЛЮБИЛ")
 
-    local Section = Tab:NewSection("KYKYRYZ0B CREATOR THIS LEAKING")
+    local Section = Tab:NewSection("Я ДОБЛАЁБ")
 
     Section:NewButton("sb test", "INFO", function()
     game:GetService("TeleportService"):Teleport(9020359053)
     end)
 
-    local Section = Tab:NewSection("Новости и сливы по Slap battles stop stealing")
+    local Section = Tab:NewSection("ЕСТЬ ВОПРОСЫ - @MEi3_ZaHaRVi")
 
     Section:NewButton("Inf Yeld", "INFO", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
     end)
 
+    Library:ToggleUIGui({
+      Icons = "rbxassetid://4384403532"
+})
+
 elseif game.PlaceId == 9020359053 then
 
 	    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Kavo.lua"))()
 
-	local Window = Library.CreateLib("stop stealing my leaked", "DarkTheme")
+	local Window = Library.CreateLib("МОЙ ЮЗЕР В ТГ @MEi3_ZaHaRVi", "DarkTheme")
 
-	local Tab = Window:NewTab("I AM KYKYRYZ0B")
+	local Tab = Window:NewTab("ЧАТ Я ЛЮБЛЮ ВАШИХ МАМ")
 
-    local Section = Tab:NewSection("KYKYRYZ0B CREATOR THIS LEAKING")
+    local Section = Tab:NewSection("Я ДОБЛАЁБ")
 
     Section:NewButton("free cam", "INFO", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/SB%20Freecam"))()
     end)
 
-    local Section = Tab:NewSection("Новости и сливы по Slap battles гавно")
+    local Section = Tab:NewSection("Я МИЛКХАНТЕР")
 
     Section:NewButton("Dex v2", "INFO", function()
     loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
     end)
 
-    local Section = Tab:NewSection("MA348 stop stealing")
+    local Section = Tab:NewSection("ВЫ МНЕ ТАК ПРИЯТНО СОСЁТЕ")
 
     Section:NewButton("Dex v4", "INFO", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
     end)
 
-    local Section = Tab:NewSection("DONT STEALING MY LEAKED")
+    local Section = Tab:NewSection("ВАШИ МАМЫ СЕКСИ")
 
     Section:NewButton("rejoin", "INFO", function()
     game:GetService("TeleportService"):Teleport(9020359053)
     end)
 
-    local Section = Tab:NewSection("WHY YOU STEALING")
+    local Section = Tab:NewSection("ВАМ СЛОВО НЕ ДАВАЛИ НЕ ПИСАТЬ СДЕСЬ")
 
     Section:NewButton("back to sb", "INFO", function()
     game:GetService("TeleportService"):Teleport(6403373529)
